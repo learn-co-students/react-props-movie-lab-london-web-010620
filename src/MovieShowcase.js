35991c06704d83c5df2ac12e27a4099f3e3f7321
pixulainen <george.apetrei@gmail.com>
2020-03-09 12:58:25 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
+	generateMovieCards = (movieData) => {
+		return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+	};
 
-  generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
-
-  render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
-  }
+	render() {
+		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+	}
 }
+// {
+//   h3 title: 'Choux and Maru go to Istanbul',
+//   span IMDBRating: 3,
+//   h5 genres: ['cats', 'adventure', 'romance'],
+//   img poster: 'choux-maru-istanbul'
+// },
