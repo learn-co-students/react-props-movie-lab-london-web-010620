2330a505103bf4712053b785bed436aa5f79ceb7
pixulainen <george.apetrei@gmail.com>
2020-03-09 13:13:53 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,7 +3,7 @@ import MovieCard from './card-components/MovieCard.js';
 import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
-	generateMovieCards = (movieData) => {
+	generateMovieCards = () => {
 		return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
 	};
 
@@ -11,9 +11,3 @@ export default class MovieShowcase extends Component {
 		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
 	}
 }
-// {
-//   h3 title: 'Choux and Maru go to Istanbul',
-//   span IMDBRating: 3,
-//   h5 genres: ['cats', 'adventure', 'romance'],
-//   img poster: 'choux-maru-istanbul'
-// },
